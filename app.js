bec9b36c560476a6c01500a3508e22046b9e9066
TomasD1998 <tomidistefano@hotmail.com>
2023-07-25 11:54:53 -0300

    Formularios terminados.

--- app.js
+++ app.js
@@ -16,6 +16,10 @@ app.get("/register", (req, res) => {
   res.sendFile(__dirname + "/views/register.html");
 });
 
+app.get("/login", (req, res) => {
+  res.sendFile(__dirname + "/views/login.html");
+});
+
 app.listen(puerto, () => {
   console.log("Aplicación esuchando en puerto 3030");
 });
